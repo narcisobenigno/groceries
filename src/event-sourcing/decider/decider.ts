d13bafb9958f1536ba598cf69788f82ac5cd2f94
Narciso <narciso.benigno@gmail.com>
2025-04-21 19:41:42 +1000

    refactor(groceries): import eventstore as package

--- src/event-sourcing/decider/decider.ts
+++ src/event-sourcing/decider/decider.ts
@@ -1,4 +1,4 @@
-import type { Envelope, Event, PersistedEnvelope } from "../event-store";
+import type { eventstore } from "@/event-sourcing";
 
 export type Command = {
   type: string;
@@ -6,8 +6,8 @@ export type Command = {
 
 export type State = Record<string, any>;
 
-export interface Decider<C extends Command, S extends State, E extends Event> {
-  decide: (command: C, state: S) => Promise<Array<Envelope<E>>>;
-  evolve: (state: S, event: PersistedEnvelope<E>) => S;
+export interface Decider<C extends Command, S extends State, E extends eventstore.Event> {
+  decide: (command: C, state: S) => Promise<Array<eventstore.Envelope<E>>>;
+  evolve: (state: S, event: eventstore.PersistedEnvelope<E>) => S;
   intialState: () => S;
 }
